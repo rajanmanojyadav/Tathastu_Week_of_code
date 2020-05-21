bd5f113c7bde0a1a52e012fa6a45e376a5541c23
Rajan Yadav <65563579+rajanmanojyadav@users.noreply.github.com>
2020-05-21 22:24:58 +0530

    Create Program2.py

---
+++ Day3/Program2.py
@@ -0,0 +1,18 @@
+# How do you find all the permutation of a String?
+
+def permutation(List, String = ""):
+    Set = set(List)
+    stringList = []
+    if len(Set) == 1:
+        String += "".join(List)
+        return list([String])
+    for x in Set:
+        List1 = list(List)
+        S = String + x
+        List1.remove(x)
+        stringList.extend(permutation(List1, S))
+    return stringList
+
+string = input("Enter a string: ")
+List = permutation(list(string))
+print("All the permutation of given string is: " + ", ".join(List))
