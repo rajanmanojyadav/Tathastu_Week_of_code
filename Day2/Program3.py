41aff3e42464cdde84fe1880fbe22a27ae05b36e
Rajan Yadav <65563579+rajanmanojyadav@users.noreply.github.com>
2020-05-21 14:16:09 +0530

    Create Program3.py

---
+++ Day2/Program3.py
@@ -0,0 +1,16 @@
+# print th following pattern:
+#     *      *
+#      *    *
+#       *  *
+#        **
+#        **  
+#       *  *
+#      *    *
+#     *      *   
+
+
+n = int(input("Enter the value of N: "))
+for i in range(n):
+    print(" " * i + "*" + "  " * (n - 1 - i) + "*")
+for i in range(n):
+    print(" " * (n - 1 - i) + "*" + "  " * i + "*")
