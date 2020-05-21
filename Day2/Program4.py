81939c789a6135618344e54ca6efbca051191cd5
Rajan Yadav <65563579+rajanmanojyadav@users.noreply.github.com>
2020-05-21 14:21:43 +0530

    Create Program4.py

---
+++ Day2/Program4.py
@@ -0,0 +1,17 @@
+#Print following Patten:
+#   **********
+#   ****  ****
+#   ***    ***
+#   **      **
+#   *        *
+#   **      **
+#   ***    ***
+#   ****  ****
+#   **********
+
+ 
+n = int(input("Enter the value of N: "))
+for i in range(n):
+    print(" " * i + "*" + "  " * (n - 1 - i) + "*")
+for i in range(n):
+    print(" " * (n - 1 - i) + "*" + "  " * i + "*")
