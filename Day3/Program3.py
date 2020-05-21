4982ab3d3b513118a02bb89cb939fafbfaacce3c
Rajan Yadav <65563579+rajanmanojyadav@users.noreply.github.com>
2020-05-21 22:27:54 +0530

    Create Program3.py

---
+++ Day3/Program3.py
@@ -0,0 +1,12 @@
+# How to remove the duplicate charcter from String ?
+
+def duplicate(string):
+    duplicateString = ""
+    for x in string:
+        if x not in duplicateString:
+            duplicateString += x
+    return duplicateString
+
+string = input("Enter the string: ")
+print("String after removing the duplicates is:", duplicate(string))
+
