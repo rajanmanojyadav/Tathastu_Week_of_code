1cc1ded72d3d5d8ef3d810a7e147232e86c61d57
Rajan Yadav <65563579+rajanmanojyadav@users.noreply.github.com>
2020-05-21 14:48:37 +0530

    Create Program5.py

---
+++ Day2/Program5.py
@@ -0,0 +1,14 @@
+#Program to print half diamond Number-Star pattern:
+#  3*3*3
+#  2*2
+#  1
+#  1
+#  2*2
+#  3*3*3
+
+
+N = int(input("Enter the Value of N: "))
+for i in range(N):
+    print((str(N - i) + "*") * (N - 1 - i) + str(N - i))
+for i in range(2,N + 1):
+    print((str(i) + "*") * (i - 1) + str(i))
