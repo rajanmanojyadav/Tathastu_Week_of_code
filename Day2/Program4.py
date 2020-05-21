bdc544382b789a0eb72e2a3e81a6c4a8d04181ed
Rajan Yadav <65563579+rajanmanojyadav@users.noreply.github.com>
2020-05-21 14:23:18 +0530

    Update Program4.py

--- Day2/Program4.py
+++ Day2/Program4.py
@@ -10,8 +10,8 @@
 #   **********
 
  
-n = int(input("Enter the value of N: "))
+n = int(input("Enter the Value of N: "))
 for i in range(n):
-    print(" " * i + "*" + "  " * (n - 1 - i) + "*")
-for i in range(n):
-    print(" " * (n - 1 - i) + "*" + "  " * i + "*")
+    print("* " * (n - i) + "    " * i + " *" * (n - i))
+for i in range(2,n + 1):
+    print("* " * i + "    " * (n - i) + " *" * i)
