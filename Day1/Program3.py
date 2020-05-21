48bf2d9662b1722f3229aebfd5d0b3bb1c0c34d2
Rajan Yadav <65563579+rajanmanojyadav@users.noreply.github.com>
2020-05-21 13:27:05 +0530

    Create Program3.py

---
+++ Day1/Program3.py
@@ -0,0 +1,10 @@
+#Swap 2 Variables without using a third variable
+
+num1=int(input("Enter the value of Fist Number: "))
+num2=int(input("Enter the value of Second Number: "))
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("After Swaping")
+print("Value of First Number :-",num1)
+print("Value of Second Number :-",num2)
